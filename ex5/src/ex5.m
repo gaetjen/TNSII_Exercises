2209e0987ec49ed7aeec451897c65ec4056fe47e
gaetjen <johannesgaetjen@gmail.com>
2016-05-31 11:28:58 +0200

    ex5 code done

--- ex5/src/ex5.m
+++ ex5/src/ex5.m
@@ -1,7 +1,8 @@
 % authors: Johannes Gätjen, Lorena Morton
-
-stimA = rand(1, 100) > 1/3;
-stimB = rand(1, 100) > 1/5;
+close all;
+numTrials = 100;
+stimA = rand(1, numTrials) < 1/3;
+stimB = rand(1, numTrials) < 1/5;
 reward = stimA;
 stim = [stimA;stimB];
 weights = zeros(size(stim));
@@ -11,4 +12,33 @@ for i = 1:length(weights) - 1
     errors(i) = reward(i) - weights(:, i)' * stim(:, i);
     weights(:, i+1) = weights(:, i) + stim(:, i) .* errors(i) * rate;
 end
+figure()
+ShowSequence( stim, reward, errors, weights )
+
+stimA = rand(1, numTrials) < 1/3;
+stimB = rand(1, numTrials) < 1/5;
+reward = rand(1, numTrials) < stimA/2;
+stim = [stimA;stimB];
+weights = zeros(size(stim));
+errors = zeros(1, length(weights));
+rate = 0.05;
+for i = 1:length(weights) - 1
+    errors(i) = reward(i) - weights(:, i)' * stim(:, i);
+    weights(:, i+1) = weights(:, i) + stim(:, i) .* errors(i) * rate;
+end
+figure()
+ShowSequence( stim, reward, errors, weights )
+
+stimA = rand(1, numTrials) < 1/3;
+stimB = rand(1, numTrials) < 1/5;
+reward = stimA & ~stimB;
+stim = [stimA;stimB];
+weights = zeros(size(stim));
+errors = zeros(1, length(weights));
+rate = 0.05;
+for i = 1:length(weights) - 1
+    errors(i) = reward(i) - weights(:, i)' * stim(:, i);
+    weights(:, i+1) = weights(:, i) + stim(:, i) .* errors(i) * rate;
+end
+figure()
 ShowSequence( stim, reward, errors, weights )
